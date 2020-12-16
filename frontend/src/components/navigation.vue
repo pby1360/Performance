713d4e6d4f7fd63fc062c99ea88043b72d32df2a
pby1360 <pby1360@gmail.com>
2020-12-17 00:02:24 +0900

    자식 태그 활성화 시 부모 배경색 변경 필요

--- frontend/src/components/navigation.vue
+++ frontend/src/components/navigation.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="nav">
-    <div class="logo">PERFORMANCE</div>
-    <ul
-      v-for="(item, index) in navData"
-      :key="index"
-    >
-      <li>
+    <div class="logo">
+      <span>PERFORMANCE</span>
+    </div>
+    <ul>
+      <li
+        v-for="(item, index) in navData"
+        :key="index"
+        @click="$router.push(item.path)"
+      >
         <router-link :to="item.path">
           {{ item.name }}
         </router-link>
@@ -25,21 +28,51 @@ export default {
   },
   mounted() {
   },
+  methods: {
+    click(e) {
+      console.log(e);
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
 .nav {
   background-color: #333;
-  color: white;
-  
+  // color: white;
+
   ul {
+    padding: 0;
+    margin: 0;
     li {
-      a:hover {}
-      a:focus {}
+      padding: 1rem 1rem 1rem 3rem;
+      list-style: none;
+      a {
+        text-decoration: none;
+        color: #c8c8c8;
+      }
+      a:hover {
+        color: white;
+      }
+      a.router-link-exact-active {
+        color: white;
+      }
+    }
+    li:hover {
+      background-color: #505050;
+    }
+    li:has(a.rounter-link-exact-active) {
+      background-color: #505050;
     }
   }
 }
-.logo {}
+.logo {
+  font-size: 1.5rem;
+  padding: 2rem;
+  font-weight: bold;
+  background-color: #22609D;
+  color: white;
+  text-align: center;
+}
 
 </style>
