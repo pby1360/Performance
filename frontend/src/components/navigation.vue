ae13ff80881a1716c09c9df2609ce345a9ef7b63
bb17 <pby1360@gmail.com>
2020-12-16 21:17:21 +0900

    test

--- frontend/src/components/navigation.vue
+++ frontend/src/components/navigation.vue
@@ -43,3 +43,48 @@ export default {
 .logo {}
 
 </style>
+<template>
+  <div class="nav">
+    <div class="logo">PERFORMANCE</div>
+    <ul
+      v-for="(item, index) in navData"
+      :key="index"
+    >
+      <li>
+        <router-link :to="item.path">
+          {{ item.name }}
+        </router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import router from '../router';
+
+export default {
+  data() {
+    return {
+      navData: router.options.routes,
+    };
+  },
+  mounted() {
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.nav {
+  background-color: #333;
+  color: white;
+  
+  ul {
+    li {
+      a:hover {}
+      a:focus {}
+    }
+  }
+}
+.logo {}
+
+</style>
